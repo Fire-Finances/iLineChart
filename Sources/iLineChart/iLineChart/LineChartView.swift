4965ef69e9138b47aaada27c4117ee9b2ed70d7e
Alton Fong <altonfong@altonfong.com>
2022-02-27 20:49:27 -0800

    Convert value to currency Value

--- Sources/iLineChart/iLineChart/LineChartView.swift
+++ Sources/iLineChart/iLineChart/LineChartView.swift
@@ -140,22 +140,22 @@ struct LineChartView: View {
                                 //     .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.textColor : self.style.textColor)
                                 if (self.showIndicatorDot) {
                                     if (self.internalRate != nil) {
-                                        Text("\(String(format: self.valueSpecifier, self.currentValue))")
+                                        Text("\(String(self.currentValue.toCurrencyValue()))")
                                         .font(self.titleFont)
                                         .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.textColor : self.style.textColor)
                                     } else {
-                                        Text("\(String(format: self.valueSpecifier, self.currentValue))")
+                                        Text("\(String(self.currentValue.toCurrencyValue()))")
                                         .font(self.titleFont)
                                         .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.textColor : self.style.textColor)
                                     }
                                 } else if (self.rawData.last != nil) {
                                     if (self.internalRate != nil) {
                                         // Text("\(String(format: self.valueSpecifier, self.rawData.last!))").font(self.priceFont)
-                                        Text("\(String(format: self.valueSpecifier, self.rawData.last!))")
+                                        Text("\(String(self.rawData.last!.toCurrencyValue()))")
                                         .font(self.titleFont)
                                         .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.textColor : self.style.textColor)
                                     } else {
-                                        Text("\(String(format: self.valueSpecifier, self.rawData.last!))")
+                                        Text("\(String(self.rawData.last!.toCurrencyValue()))")
                                         .font(self.titleFont)
                                         .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.textColor : self.style.textColor)
                                     }
@@ -268,5 +268,16 @@ struct WidgetView_Previews: PreviewProvider {
             LineChartView(data: [282.502, 284.495, 283.51, 285.019, 285.197, 286.118, 288.737, 288.455, 289.391, 287.691, 285.878, 286.46, 286.252, 284.652, 284.129, 284.188], title: "Line chart", legend: "Basic")
             .environment(\.colorScheme, .light)
         }
+    }
+}
+
+
+extension Double {
+    public func toCurrencyValue() -> String {
+        let value = Double(self)
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        let formattedValue = formatter.string(from: value as NSNumber)
+        return formattedValue ?? ""
     }
 }
